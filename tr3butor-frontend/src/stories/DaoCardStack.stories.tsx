ce5eb5e6eabc524e0f8205576416435acadc7f84
Oleksii Sysoiev <bostonleek@gmail.com>
2021-12-16 16:07:31 +0200

    fix dao card stack story

--- tr3butor-frontend/src/stories/DaoCardStack.stories.tsx
+++ tr3butor-frontend/src/stories/DaoCardStack.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { DaoCardStack } from '../components/DaoCardStack/DaoCardStack'
+import daoData from 'api/mirage/Fixtures/getDao'
+import { DaoParams } from '../types/dao'
 
 export default {
   title: 'tr3butor/DaoCardStack',
@@ -13,11 +15,13 @@ const Template: ComponentStory<typeof DaoCardStack> = (args) => (
 
 export const Vertical = Template.bind({})
 Vertical.args = {
-  type: 'vertical'
+  type: 'vertical',
+  data: daoData as unknown as DaoParams[]
 }
 
 export const Horizontal = Template.bind({})
 
 Horizontal.args = {
-  type: 'horizontal'
+  type: 'horizontal',
+  data: daoData as unknown as DaoParams[]
 }
